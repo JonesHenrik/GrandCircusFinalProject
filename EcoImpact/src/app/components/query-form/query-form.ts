554cdd9a8ecb2b14e5b150f12bbc7b88ceacd0b1
Kiana Bauer <bauerkia@msu.edu>
2025-08-28 19:02:33 -0400

    interfaces

---
+++ EcoImpact/src/app/components/query-form/query-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-query-form',
+  imports: [],
+  templateUrl: './query-form.html',
+  styleUrl: './query-form.css'
+})
+export class QueryForm {
+
+}
