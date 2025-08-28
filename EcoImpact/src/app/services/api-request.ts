b4dad7d78af0a9b431375d512583d07c7fc14a5e
Marquise Adeleye <marquiseadeleye@gmail.com>
2025-08-28 19:32:36 -0400

    Api Server

--- EcoImpact/src/app/services/api-request.ts
+++ EcoImpact/src/app/services/api-request.ts
@@ -6,5 +6,14 @@ import { Injectable } from '@angular/core';
 export class ApiRequest {
   private theServerURL : string = "https://api.openai.com/v1/chat/completions"
 
+  thePollutionResults : any[] = []
+
+  constructor(private theServer:HttpClient) {}
+
+  async getPollutionResults() : Promise<any[]> {
+    const result : any[] = await lastValueFrom(this.theServer.get<any[]>(this.theServerURL))
+    return result;
+    }
+
   
 }
