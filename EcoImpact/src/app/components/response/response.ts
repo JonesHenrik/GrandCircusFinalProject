56323c22a7ece989c1990495c0a1533dec4b9215
Marquise Adeleye <marquiseadeleye@gmail.com>
2025-09-12 19:39:59 -0400

    link data

--- EcoImpact/src/app/components/response/response.ts
+++ EcoImpact/src/app/components/response/response.ts
@@ -3,7 +3,7 @@ import {ApiRequest} from '../../services/api-request';
 import { CommonModule } from '@angular/common';
 import { FormsModule }   from '@angular/forms';
 import { Record } from '../../interfaces/record';
-
+import {DatabaseService} from '../../services/database-service';
 
 @Component({
   selector: 'app-response',
@@ -17,17 +17,33 @@ export class Response {
   //Initially null since no values are used yet
   impact: {water: number, electricity: number, carbon: number, response: string} | null = null;
  // impactHistory is meant to store the total amount of waste and response to later be added to the database
-  impactHistory: {response: Record["response"]; water: Record["waterW"]; electricity: Record["electricW"]; carbon: Record["co2W"]}[] =[]
+  impactHistory: Record[] = [];
+  AllTotals : {waterTotal : 0; electricTotal : 0; carbonTotal : 0};
 
 
-  constructor(private apiService: ApiRequest) {}
+  record: Record = {
+      id : 0,
+      response : "",
+      waterW : 0,
+      electricW : 0,
+      co2W : 0,
+      version : 0
+    }
+
+
+  constructor(private apiService: ApiRequest, private database: DatabaseService) {}
 
   sendMessage(){
     this.apiService.sendMessage(this.userInput).subscribe({
       next: (data) => {
         this.impact = data; //stores environmental impact results
-        this.impactHistory.push(data); // Adding to impactHistory array
-
+        this.record.id = 99;
+        this.record.response = this.impact.response;
+        this.record.waterW = this.impact.water;
+        this.record.electricW = this.impact.electricity;
+        this.record.co2W = this.impact.carbon;
+        this.record.version = 99;
+        this.database.addRecord(this.record);
         },
       error: (err) => {
         console.error('Error', err);
@@ -37,14 +53,30 @@ export class Response {
     }
 
   get totalImpact(){
-    return this.impactHistory.reduce(
-      (totals, current) => {
-        totals.water += current.water;
-        totals.electricity += current.electricity;
-        totals.carbon += current.carbon;
-        return totals;
+    this.impactHistory = this.database.getAllRecords();
+    this.waterTotal = this.impactHistory.reduce(
+      (theTotal, current) => {
+        theTotal += current.waterW;
+        return theTotal;
         },
-      {water: 0, electricity: 0, carbon: 0} //Initial totals
+      {theTotal: 0} //Initial totals
       );
+    this.electricTotal = this.impactHistory.reduce(
+          (theTotal, current) => {
+            theTotal += current.electricW;
+            return theTotal;
+            },
+          {theTotal: 0} //Initial totals
+          );
+    this.carbonTotal = this.impactHistory.reduce(
+              (theTotal, current) => {
+                theTotal += current.co2W;
+                return theTotal;
+                },
+              {theTotal: 0} //Initial totals
+              );
+
+
+    return this.AllTotals
     }
 }
